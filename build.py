08404320018b723ce9aed209644f680fe3115902
LimHM <gusals3407@gmail.com>
2017-04-06 17:53:27 +0900

    docker conf base2추가

--- build.py
+++ build.py
@@ -1,18 +1,24 @@
 import argparse
 import os
-
-# Const
 import subprocess
 import sys
 
+# Const
 MODE_BASE = 'base'
+MODE_BASE2 = 'base2'
 MODE_DEBUG = 'debug'
 MODE_PRODUCTION = 'production'
+MODE_DOCKERHUB = 'dokcerhub'
+
 IMAGE_BASE = 'front-base'
+IMAGE_BASE2 = 'front-base2'
 IMAGE_DEBUG = 'front-debug'
 IMAGE_PRODUCTION = 'front'
-MAINTAINER = 'dev@abc.com'
+
+
+MAINTAINER = 'dev@azelf.com'
 DOCKERFILE_BASE = 'Dockerfile.base'
+DOCKERFILE_BASE2 = 'Dockerfile.base2'
 DOCKERFILE_DEBUG = 'Dockerfile.debug'
 DOCKERFILE_PRODUCTION = 'Dockerfile'
 
@@ -26,9 +32,10 @@ ROOT_DIR = os.path.dirname(__file__)
 CONF_DIR = os.path.join(ROOT_DIR, '.conf')
 CONF_DOCKER_DIR = os.path.join(CONF_DIR, 'docker')
 
-# Docker conf file
+# Docker conf files
 dockerfile_template = open(os.path.join(CONF_DOCKER_DIR, '00_template.docker')).read()
 dockerfile_base = open(os.path.join(CONF_DOCKER_DIR, '01_base.docker')).read()
+dockerfile_base2 = open(os.path.join(CONF_DOCKER_DIR, '01_base2.docker')).read()
 dockerfile_extra = open(os.path.join(CONF_DOCKER_DIR, '02_extra.docker')).read()
 
 if args.mode == MODE_BASE:
@@ -40,9 +47,18 @@ if args.mode == MODE_BASE:
     )
     filename = DOCKERFILE_BASE
     imagename = IMAGE_BASE
-elif args.mode == MODE_DEBUG:
+elif args.mode == MODE_BASE2:
     dockerfile = dockerfile_template.format(
         from_image=IMAGE_BASE,
+        maintainer=MAINTAINER,
+        base=dockerfile_base2,
+        extra=''
+    )
+    filename = DOCKERFILE_BASE2
+    imagename = IMAGE_BASE2
+elif args.mode == MODE_DEBUG:
+    dockerfile = dockerfile_template.format(
+        from_image=IMAGE_BASE2,
         maintainer=MAINTAINER,
         base='',
         extra=dockerfile_extra
