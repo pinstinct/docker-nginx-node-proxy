9e51cbf8a399527429f7dfafb852316db830c8ed
LimHM <gusals3407@gmail.com>
2017-04-11 15:43:44 +0900

    CeleryTest create_at필드 default값 지정

--- django_app/member/admin.py
+++ django_app/member/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from member.models import CeleryTest
 
+
+class CeleryTestAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'created_at',
+    )
+
 admin.site.register(CeleryTest)
